bbbfdc778c5a1676cc9b0d13d9d95419722eeab1
DJ <DJ@DESKTOP-GN8M4PF>
2024-08-29 19:43:59 +0200

    Add project files.

---
+++ Relaxdays_Test_Djordje_Tepic/Controllers/Aufgabe_Controller.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.Json;
+
+namespace Relaxdays_Test_Djordje_Tepic.Controllers
+{
+    [Route("api/api/v1/createDatabase")]
+    [ApiController]
+    public class Aufgabe_Controller : ControllerBase
+    {
+        [HttpPost]
+        public DatenbankReturn Main(string Datenbankname)
+        {
+            string b = GetPassword();
+            var liste = DateiEinlesen();
+
+            Datenbank ret = new Datenbank()
+            {
+                dbName = $"{Datenbankname}",
+                dbUser = $"task{Datenbankname}mysql",
+                dbPasswort = $"{b}"
+            };
+            liste.Add(ret);
+            string jsonString = JsonSerializer.Serialize(liste);
+            System.IO.File.WriteAllText("Datei.json", jsonString);
+
+
+
+            DatenbankReturn kreatur = new DatenbankReturn()
+            {
+                dbPasswort = $"{b}",
+                dbUser = $"task{Datenbankname}mysql"
+            };
+            return kreatur;
+
+        }
+
+        private string GetPassword()
+        {
+            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var Charsarr = new char[32];
+            var random = new Random();
+
+            for (int i = 0; i < Charsarr.Length; i++)
+            {
+                Charsarr[i] = characters[random.Next(characters.Length)];
+            }
+
+            var resultString = new String(Charsarr);
+
+            return resultString;
+
+        }
+        
+        public static List<Datenbank> DateiEinlesen()     //Auswahl 5; object vor DateiEinlesen?
+        {
+            List<Datenbank> ob = new List<Datenbank>();                          // object ob = null;?
+
+            try
+            {
+
+                string JsonContent = System.IO.File.ReadAllText("Datei.json");   //Datei Einlesen system.IO...
+
+                //Json-Datei einlesen
+                return JsonSerializer.Deserialize<List<Datenbank>>(JsonContent); // generische Klasse --> mehere Typen, mit den Klammern welcher Typ
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return ob;
+        }
+    }
+
+
+
+
+
+}
---
+++ Relaxdays_Test_Djordje_Tepic/WeatherForecast.cs
@@ -0,0 +1,18 @@
+namespace Relaxdays_Test_Djordje_Tepic
+{
+    public class Datenbank
+    {
+        public string dbName { get; set; }
+        public string dbUser { get; set; }
+        public string dbPasswort { get; set; }
+
+    }
+
+    public class DatenbankReturn
+    { 
+        public string dbUser { get; set; }
+        public string dbPasswort { get; set; }
+
+    }
+
+}
